a15f73ab828b610b1cd9f6708b09ee125657c927
Prima Aulia Gusta <primaulia@gmail.com>
2021-02-23 14:18:15 +0800

    latest about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
   devise_for :users , path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :flats, only: [:index]
 end
